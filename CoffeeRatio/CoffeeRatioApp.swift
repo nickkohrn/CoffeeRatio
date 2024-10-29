67a5a91b08a3c4dd5982bc6ada430d693cc93f2c
Nick Kohrn <bnkohrn@icloud.com>
2024-10-29 09:37:55 -0400

    Add project files

---
+++ CoffeeRatio/CoffeeRatioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeRatioApp.swift
+//  CoffeeRatio
+//
+//  Created by Nick Kohrn on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeRatioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
